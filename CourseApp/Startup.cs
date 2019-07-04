267c0219e9dcf1bcc11a2699cd7fae4e465be33c
yunus emre demir <yunus.emredemir222002@gmail.com>
2019-07-04 12:04:24 +0300

    Bootstrap ile arayüz
    Bootstap kullanarak arayüzümüzü eli yüzü düzgün bir hale getirdik
    npm i bootstrap

--- CourseApp/Startup.cs
+++ CourseApp/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 
 namespace CourseApp
 {
@@ -23,6 +25,16 @@ namespace CourseApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles(); //wwwroot
+            
+            app.UseStaticFiles(new StaticFileOptions
+             {
+                FileProvider= new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),"node_modules")),
+                    RequestPath = "/modules"
+            });
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
