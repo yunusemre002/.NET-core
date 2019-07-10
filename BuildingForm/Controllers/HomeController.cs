1299f53c0e960a1277fa887a14b5738516751964
yunus emre demir <yunus.emredemir222002@gmail.com>
2019-07-10 13:23:11 +0300

    1. Step : Creating
    We just create a project which include what it need.
    I can say that is template  and I should use all of steps to create any project.

---
+++ BuildingForm/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuildingForm.Controllers
+{
+    public class HomeController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }public IActionResult Create()
+        {
+            return View();
+        }public IActionResult Search()
+        {
+            return View();
+
+    }
+}
---
+++ BuildingForm/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace BuildingForm.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string  Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool isApproved { get; set; }
+    }
+}
